9c93b730e588e8f82393b73712882e4d3da4f52f
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-11-20 22:25:53 +0600

    migration

---
+++ database/migrations/2023_11_20_151530_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->from(startingValue: 1000);
+            $table->string(column: 'title')->unique();
+            $table->string(column: 'slug')->unique();
+            $table->text(column: 'excerpt')->comment(comment: 'Summary of the post');
+            $table->longText(column: 'description');
+            $table->boolean(column: 'is_published')->default(value: false);
+            $table->integer(column: 'min_to_read')->nullable(value: false);
+            $table->timestamps();
+
+
+            /*We can also assign unique column as below if we dont want to chaining*/
+//            $table->unique('title');
+//            $table->unique(['title', 'slug']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+require __DIR__.'/migration.php';
 
+
+
+/*==============================*/
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome";
 });
